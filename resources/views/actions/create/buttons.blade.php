339070a85b9dd418391a7bc1897447f1cc5a58ca
Daniel (TMP) Seuffer <authanram@gmail.com>
2019-12-28 05:36:39 +0100

    WIP

--- resources/views/actions/create/buttons.blade.php
+++ resources/views/actions/create/buttons.blade.php
@@ -1,13 +1,15 @@
 @php /** @var \Authanram\Resources\Http\Actions\EditAction $action */ @endphp
 
 <button
-    class="{{ $action->theme('button.default', 'button.primary') }} mr-2"
+    class="{{ $action->theme('buttons.accents.default', 'buttons.accents.primary') }} mr-2"
 
     name="_action"
 
     type="submit"
 
     value="create"
+
+    {{ $action->theme('buttons.directives') }}
 >
 
     {{ __('Create & Add Another') }}
@@ -15,9 +17,11 @@
 </button>
 
 <button
-    class="{{ $action->theme('button.default', 'button.secondary') }}"
+    class="{{ $action->theme('buttons.accents.default', 'buttons.accents.secondary') }}"
 
     type="submit"
+
+    {{ $action->theme('buttons.directives') }}
 >
 
     {{ __('Create') }} {{ $action->getResourceName() }}
--- resources/views/actions/edit/buttons.blade.php
+++ resources/views/actions/edit/buttons.blade.php
@@ -1,13 +1,15 @@
 @php /** @var \Authanram\Resources\Http\Actions\EditAction $action */ @endphp
 
 <button
-    class="{{ $action->theme('button.default', 'button.primary') }} mr-2"
+    class="{{ $action->theme('buttons.accents.default', 'buttons.accents.primary') }} mr-2"
 
     name="_action"
 
     type="submit"
 
     value="edit"
+
+    {{ $action->theme('buttons.directives') }}
 >
 
     {{ __('Update & Continue Editing') }}
@@ -15,9 +17,11 @@
 </button>
 
 <button
-    class="{{ $action->theme('button.default', 'button.secondary') }}"
+    class="{{ $action->theme('buttons.accents.default', 'buttons.accents.secondary') }}"
 
     type="submit"
+
+    {{ $action->theme('buttons.directives') }}
 >
 
     {{ __('Update') }} {{ $action->getResourceName() }}
