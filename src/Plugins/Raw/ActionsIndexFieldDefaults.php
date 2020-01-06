9c5db52bfe131f45d9520f03159fda9b1e6f0284
Daniel (TMP) Seuffer <authanram@gmail.com>
2020-01-06 23:03:02 +0100

    WIP

--- src/Plugins/Raw/ActionsIndexFieldDefaults.php
+++ src/Plugins/Raw/ActionsIndexFieldDefaults.php
@@ -8,45 +8,14 @@ final class ActionsIndexFieldDefaults implements RawPluginContract
 {
     public function handle(\stdClass $resource): \stdClass
     {
-        $indexFields = (array)data_get($resource, 'actions.index.fields', (object)[]);
+        $fields = data_get($resource, 'fields');
 
-        if ($indexFields) {
+        if (empty ($fields)) {
 
-            return $resource;
-
-        }
-
-        $rawFields = get_object_vars($resource->fields);
-
-        $fields = static::makeIndexFields($rawFields);
-
-        return data_set($resource, 'actions.index.fields', (object)$fields);
-    }
-
-    private static function makeIndexFields(array $fields): array
-    {
-        $indexFieldsAll = [];
-
-        $fn = fn ($field, $key) => $key !== 'id';
-
-        $filteredFields = collect($fields)->filter($fn);
-
-        foreach ($filteredFields->toArray() as $key => $field) {
-
-            $indexFieldsAll[$key] = $field;
-
-            $indexFieldsAll[$key]->attribute = $key;
+            $resource->fields = (object)[];
 
         }
 
-        $fn = fn (\stdClass $field) => \in_array($field->attribute, ['created_at', 'updated_at']);
-
-        return array_merge(
-
-            collect($indexFieldsAll)->take(3)->toArray(),
-
-            $filteredFields->filter($fn)->toArray()
-
-        );
+        return $resource;
     }
 }
--- src/Plugins/Raw/ResourceFieldDefaults.php
+++ src/Plugins/Raw/ResourceFieldDefaults.php
@@ -3,110 +3,13 @@
 namespace Authanram\Resources\Plugins\Raw;
 
 use Authanram\Resources\Contracts\RawPluginContract;
-use Authanram\Resources\Entities\Association;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 final class ResourceFieldDefaults implements RawPluginContract
 {
     public function handle(\stdClass $resource): \stdClass
     {
-        /** @var Model $instance */
-        $instance = new $resource->model;
-
-        $table = $instance->getTable();
-
-        $columnsDefinitions = static::getColumnDefinitions($table);
-
-        $fields = static::makeResourceFields($columnsDefinitions);
-
-        $fieldsCurrent = data_get($resource, 'fields', (object)[]);
-
-        $resource->fields = (object)array_merge(
-
-            (array)static::setAssociationsToFields($fields, $instance),
-
-            (array)$fieldsCurrent
-
-        );
-
         return $resource;
-    }
-
-    private static function setAssociationsToFields(array $fields, Model $instance): \stdClass
-    {
-        $replace = [];
-
-        foreach ($fields as $key => $value) {
-
-            if (! Str::endsWith($key, '_id')) {
-
-                continue;
-
-            }
-
-            $segment = rtrim($key, '_id');
-
-            $method = Str::camel($segment);
-
-            if (! method_exists($instance, $method)) {
-
-                continue;
-
-            }
-
-            $fields[$key]->type = Association::BELONGS_TO;
-
-            $replace[$key] = $method;
-
-        }
-
-        $json = json_encode($fields, JSON_THROW_ON_ERROR, 512);
-
-        foreach ($replace as $key => $value) {
-
-            $json = str_replace($key, $value, $json);
-
-        }
-
-        return json_decode($json, false, 512, JSON_THROW_ON_ERROR);
-    }
-
-    private static function makeResourceFields(array $columns): array
-    {
-        return collect($columns)->mapWithKeys(static function (\stdClass $column) {
-
-            $type = Str::before($column->Type, '(');
-
-            $fieldType = static::makeFieldType($type);
-
-            $field = [
-
-                'attribute' => $column->Field,
-
-                'length' => static::getColumnLength($column->Type),
-
-                'nullable' => $column->Null === 'Yes',
-
-                'type' => $fieldType,
-
-            ];
-
-            return [$column->Field => (object)$field];
-
-        })->toArray();
-    }
-
-    private static function getColumnLength(string $type): ?int
-    {
-        preg_match('/\((.*?)\)/', $type, $matches);
-
-        return ! empty($matches[1]) && \is_numeric($matches[1])
-
-            ? $matches[1]
-
-            : null;
     }
 
     private static function getColumnDefinitions(string $table): array
